4e93b8157a165242c360f96689bd3046435da335
Taylor.Ferguson <tayfer95@gmail.com>
2022-01-30 17:23:14 -0800

    breaking up function for easier testing, writing more tests

--- src/lexxer.rs
+++ src/lexxer.rs
@@ -5,6 +5,8 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::VecDeque;
 
+const TOKENS_NEED_SPACE_REGEX : &str = r"[\{\}\(\);!~-]";
+
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -12,6 +14,16 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+fn format_spacing(string : &str) -> String
+{
+    let re = Regex::new(TOKENS_NEED_SPACE_REGEX).unwrap();
+    return re.replace_all(&string," $0 ").into_owned();
+}
+
+fn from_line(line : &str) -> VecDeque<LexToken>
+{
+    return format_spacing(line).split_whitespace().map(|word| LexToken::from_str(word)).collect();
+}
 
 #[derive(Debug,PartialEq,Eq,Clone)]
 pub enum LexToken
@@ -81,6 +93,7 @@ impl LexToken
 }
 
 
+
 pub fn lex(file_path : &str) -> VecDeque<LexToken> 
 {
     let mut lex_vec = VecDeque::new();
@@ -88,12 +101,9 @@ pub fn lex(file_path : &str) -> VecDeque<LexToken>
     {
         for line in lines
         {
-            if let Ok(l) = line 
+            if let Ok(l) = line
             {
-                let re = Regex::new(r"[\{\}\(\);!~-]").unwrap();
-                let space_l = re.replace_all(&l," $0 ").into_owned();
-                let mut lex_line : VecDeque<LexToken> = space_l.split_whitespace().map(|word| LexToken::from_str(word)).collect();
-                lex_vec.append(&mut lex_line);
+                lex_vec.append(&mut from_line(&l));
             }
         }
     }
@@ -101,39 +111,110 @@ pub fn lex(file_path : &str) -> VecDeque<LexToken>
 }
 
 
-#[test]
-fn test_from_str() 
+#[cfg(test)]
+mod test
 {
-    assert_eq!(LexToken::OpenBrace, LexToken::from_str("{"));
-    assert_eq!(LexToken::CloseBrace, LexToken::from_str("}"));
-    assert_eq!(LexToken::OpenParenth, LexToken::from_str("("));
-    assert_eq!(LexToken::CloseParenth, LexToken::from_str(")"));
-    assert_eq!(LexToken::Semicolon, LexToken::from_str(";"));
-    assert_eq!(LexToken::Int, LexToken::from_str("int"));
-    assert_eq!(LexToken::Return, LexToken::from_str("return"));
-    assert_eq!(LexToken::Negation, LexToken::from_str("-"));
-    assert_eq!(LexToken::BitwiseComplement, LexToken::from_str("~"));
-    assert_eq!(LexToken::LogicalNegation, LexToken::from_str("!"));
-    assert_eq!(LexToken::Identifier(String::from("main")), LexToken::from_str("main"));
-    assert_eq!(LexToken::IntLiteral(String::from("99")), LexToken::from_str("99"));
-    assert_eq!(LexToken::Undefined, LexToken::from_str("..."));
+    use super::*;
+
+fn generate_stage_2_vec(un_oper : LexToken) -> VecDeque<LexToken>
+{
+    let mut stage2_vec = VecDeque::new();
+    stage2_vec.push_back(LexToken::Int);
+    stage2_vec.push_back(LexToken::Identifier(String::from("main")));
+    stage2_vec.push_back(LexToken::OpenParenth);
+    stage2_vec.push_back(LexToken::CloseParenth);
+    stage2_vec.push_back(LexToken::OpenBrace);
+    stage2_vec.push_back(LexToken::Return);
+    stage2_vec.push_back(un_oper);
+    stage2_vec.push_back(LexToken::IntLiteral(String::from("3")));
+    stage2_vec.push_back(LexToken::Semicolon);
+    stage2_vec.push_back(LexToken::CloseBrace);
+    return stage2_vec;
 }
 
-#[test]
-fn test_to_str() 
-{
-    assert_eq!(LexToken::OpenBrace.to_str(), Some("{"));
-    assert_eq!(LexToken::CloseBrace.to_str(), Some("}"));
-    assert_eq!(LexToken::OpenParenth.to_str(), Some("("));
-    assert_eq!(LexToken::CloseParenth.to_str(), Some(")"));
-    assert_eq!(LexToken::Semicolon.to_str(), Some(";"));
-    assert_eq!(LexToken::Int.to_str(), Some("int"));
-    assert_eq!(LexToken::Return.to_str(), Some("return"));
-    assert_eq!(LexToken::Negation.to_str(), Some("-"));
-    assert_eq!(LexToken::BitwiseComplement.to_str(), Some("~"));
-    assert_eq!(LexToken::LogicalNegation.to_str(), Some("!"));
-    assert_eq!(LexToken::Identifier(String::from("main")).to_str(), Some("main"));
-    assert_eq!(LexToken::IntLiteral(String::from("99")).to_str(), Some("99"));
+    #[test]
+    fn test_simple_format_spacing()
+    {
+        assert_eq!(format_spacing("hello;"), "hello ; ");
+        assert_eq!(format_spacing("hello}"), "hello } ");
+        assert_eq!(format_spacing("hello{"), "hello { ");
+        assert_eq!(format_spacing("hello)"), "hello ) ");
+        assert_eq!(format_spacing("hello("), "hello ( ");
+        assert_eq!(format_spacing("!hello"), " ! hello");
+        assert_eq!(format_spacing("~hello"), " ~ hello");
+        assert_eq!(format_spacing("-hello"), " - hello");
+    }
+    
+    #[test]
+    fn test_format_spacing()
+    {
+        assert_eq!(format_spacing("-!~{hello"), " -  !  ~  { hello");
+        assert_eq!(format_spacing("void main(){return 3;}"), "void main (  )  { return 3 ;  } ");
+        assert_eq!(format_spacing("int main(int argc, char* argv) {return ~1; }"),
+                     "int main ( int argc, char* argv )   { return  ~ 1 ;   } ");
+    }
+    
+    
+    #[test]
+    fn test_stage1_from_line()
+    {
+        let mut stage1_vec = VecDeque::new();
+        stage1_vec.push_back(LexToken::Int);
+        stage1_vec.push_back(LexToken::Identifier(String::from("main")));
+        stage1_vec.push_back(LexToken::OpenParenth);
+        stage1_vec.push_back(LexToken::CloseParenth);
+        stage1_vec.push_back(LexToken::OpenBrace);
+        stage1_vec.push_back(LexToken::Return);
+        stage1_vec.push_back(LexToken::IntLiteral(String::from("3")));
+        stage1_vec.push_back(LexToken::Semicolon);
+        stage1_vec.push_back(LexToken::CloseBrace);
+        assert_eq!(from_line("int main(){return 3;}"), stage1_vec)
+    }
 
-    assert_eq!(LexToken::Undefined.to_str(),None);
+    #[test]
+    fn test_stage2_from_line()
+    {
+        assert_eq!(from_line("int main(){return !3;}"), generate_stage_2_vec(LexToken::LogicalNegation));
+        assert_eq!(from_line("int main(){return -3;}"), generate_stage_2_vec(LexToken::Negation));
+        assert_eq!(from_line("int main(){return ~3;}"), generate_stage_2_vec(LexToken::BitwiseComplement));
+    }
+
+
+    #[test]
+    fn test_from_str() 
+    {
+        assert_eq!(LexToken::OpenBrace, LexToken::from_str("{"));
+        assert_eq!(LexToken::CloseBrace, LexToken::from_str("}"));
+        assert_eq!(LexToken::OpenParenth, LexToken::from_str("("));
+        assert_eq!(LexToken::CloseParenth, LexToken::from_str(")"));
+        assert_eq!(LexToken::Semicolon, LexToken::from_str(";"));
+        assert_eq!(LexToken::Int, LexToken::from_str("int"));
+        assert_eq!(LexToken::Return, LexToken::from_str("return"));
+        assert_eq!(LexToken::Negation, LexToken::from_str("-"));
+        assert_eq!(LexToken::BitwiseComplement, LexToken::from_str("~"));
+        assert_eq!(LexToken::LogicalNegation, LexToken::from_str("!"));
+        assert_eq!(LexToken::Identifier(String::from("main")), LexToken::from_str("main"));
+        assert_eq!(LexToken::IntLiteral(String::from("99")), LexToken::from_str("99"));
+        assert_eq!(LexToken::Undefined, LexToken::from_str("..."));
+    }
+    
+    #[test]
+    fn test_to_str() 
+    {
+        assert_eq!(LexToken::OpenBrace.to_str(), Some("{"));
+        assert_eq!(LexToken::CloseBrace.to_str(), Some("}"));
+        assert_eq!(LexToken::OpenParenth.to_str(), Some("("));
+        assert_eq!(LexToken::CloseParenth.to_str(), Some(")"));
+        assert_eq!(LexToken::Semicolon.to_str(), Some(";"));
+        assert_eq!(LexToken::Int.to_str(), Some("int"));
+        assert_eq!(LexToken::Return.to_str(), Some("return"));
+        assert_eq!(LexToken::Negation.to_str(), Some("-"));
+        assert_eq!(LexToken::BitwiseComplement.to_str(), Some("~"));
+        assert_eq!(LexToken::LogicalNegation.to_str(), Some("!"));
+        assert_eq!(LexToken::Identifier(String::from("main")).to_str(), Some("main"));
+        assert_eq!(LexToken::IntLiteral(String::from("99")).to_str(), Some("99"));
+    
+        assert_eq!(LexToken::Undefined.to_str(),None);
+    }
+    
 }
